f374da14ec81c256f74b27fdbcfbc1fc47b91d33
fujino <s22t333@kagawa-u.ac.jp>
2025-08-07 16:36:45 +0900

    change to easy

--- main.go
+++ main.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-	"net/http"
+    "net/http"
 
-	"github.com/labstack/echo/v4"
+    "github.com/labstack/echo/v4"
 )
 
 func main() {
-	e := echo.New()
-	e.GET("/ping", func(c echo.Context) error {
-		return c.String(http.StatusOK, ping())
-	})
+    e := echo.New()
+    e.GET("/ping", func(c echo.Context) error {
+        return c.String(http.StatusOK, ping())
+    })
 
-	e.GET("/easy", func(c echo.Context) error {
-		return c.String(http.StatusOK, easy())
-	})
+    // aliveを追加
+    e.GET("/easy", func(c echo.Context) error {
+        return c.String(http.StatusOK, easy())
+    })
 
-	e.Logger.Fatal(e.Start(":1323"))
+    e.Logger.Fatal(e.Start(":1323"))
 }
 
 func ping() string {
-	return "pong"
+    return "pong"
 }
 
 func easy() string {
-	return "easy!!!"
+    return "easy"
 }
